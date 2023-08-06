924fb09d6985ec08e4974045654fd5d66fc63ff6
Shreya Sharma <110884745+Shreyaa710@users.noreply.github.com>
2023-08-06 21:46:24 +0530

    Time: 19 ms (97.50%), Space: 25.7 MB (14.07%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-      
-        int n=nums.size();
-        if(n>k && k!=0){
-            int temp[k],j=0;
-        for(int i=n-k;i<n;i++)
-        {
-            temp[j++]=nums[i];
+        
+        if(k>nums.size()){
+            k=(k%nums.size());
         }
-        for(int i=n-k-1;i>=0;i--)
+     
+        vector<int>temp;
+        int index=nums.size()-k;
+        for(int i=0;i<k;i++)
+        {
+            temp.push_back(nums[index++]);
+        }
+        
+        for(int i=nums.size()-k-1;i>=0;i--)
         {
             nums[i+k]=nums[i];
         }
+        
         for(int i=0;i<k;i++)
         {
             nums[i]=temp[i];
         }
-        }
-        else if(n==k || k==0){
-            for(int i=0;i<n;i++){
-                nums[i];
-            }
-        }
-        else{
-            int another[n],front;
-            for(int i=0;i<n;i++){
-                another[i]=nums[i];
-            }
-            for(int i=0;i<n;i++){
-                front=i;
-                for(int j=1;j<=k;j++){
-                    front=(i+j)%n;
-                }
-                nums[front]=another[i];
-            }
-        }
-    }
+    }
 };
