12e8cdd71d7b0ce5b031124cf5e82ede4774fc2f
Shreya Sharma <110884745+Shreyaa710@users.noreply.github.com>
2023-10-16 17:19:11 +0530

    Time: 6 ms (30.96%), Space: 12 MB (6.17%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_map<int, int> arr;
-        for(int i=0;i<nums.size();i++) arr[nums[i]]++;
-        vector<int> a;
-        for(auto e:arr) {
-            if(e.second>1) {
-                a.push_back(e.first);
-                a.push_back(e.first);
-            }
-            else{
-                a.push_back(e.first);
-            }
+        // unordered_map<int, int> arr;
+        // for(int i=0;i<nums.size();i++) arr[nums[i]]++;
+        // vector<int> a;
+        // for(auto e:arr) {
+        //     if(e.second>1) {
+        //         a.push_back(e.first);
+        //         a.push_back(e.first);
+        //     }
+        //     else{
+        //         a.push_back(e.first);
+        //     }
+        // }
+        // sort(a.begin(), a.end()) ;
+        // for(int i=0;i<a.size();i++) {
+        //     nums[i] = a[i];
+        // }
+        // return a.size() ;
+        
+        map <int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
         }
-        sort(a.begin(), a.end()) ;
-        for(int i=0;i<a.size();i++) {
-            nums[i] = a[i];
+        
+        int j=0;
+        for(auto it: m){
+          if(it.second>=2){
+                nums[j++] = it.first;
+                nums[j++] = it.first;
+          }
+            else nums[j++]=it.first;
+            
         }
-        return a.size() ;
+        return j;
     }
 };
