ba0d51ff2213d62baf230e1d29d7cad18fabb633
Shreya Sharma <110884745+Shreyaa710@users.noreply.github.com>
2023-10-16 16:42:48 +0530

    Time: 10 ms (87.99%), Space: 20 MB (29.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,16 +3,31 @@ public:
 
     int majorityElement(vector<int>& nums) {
       
-        unordered_map<int,int> m;
-        for(int i=0;i<nums.size();i++)
-        {
-            m[nums[i]]++;
-        }
+//         unordered_map<int,int> m;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             m[nums[i]]++;
+//         }
     
-        for(auto x:m){
-            if(x.second>nums.size()/2)
-                return x.first;
+//         for(auto x:m){
+//             if(x.second>nums.size()/2)
+//                 return x.first;
+//         }
+//         return -1;
+        
+        sort(nums.begin(), nums.end());
+        
+        int count =1;
+        int i;
+        for( i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {count++;
+            if(count>(nums.size()/2)) return nums[i] ;
+            }
+            else count =1;
         }
-        return -1;
+        
+        return nums[0];
     }
 };
