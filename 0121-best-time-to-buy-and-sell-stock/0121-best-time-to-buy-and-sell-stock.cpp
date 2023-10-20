560e70ac7fce55e72b8508207d6ceca662eb8ca2
Shreya Sharma <110884745+Shreyaa710@users.noreply.github.com>
2023-10-20 11:01:06 +0530

    Time: 98 ms (45.07%), Space: 93.8 MB (10.74%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     
-    // int max(int a, int b){
-    //     if(a>b){
-    //         return a;
-    //     }
-    //     return b;
-    // }
-    
     int maxProfit(vector<int>& prices) {
         int res=0,i=0;
         for(int j=1;j<prices.size();j++){
@@ -19,5 +12,7 @@ public:
             }
         }
         return res;
+        
+        
     }
 };
