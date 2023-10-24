fad5348036f20fb42c39e7bff7f095d0835b5efe
Shreya Sharma <110884745+Shreyaa710@users.noreply.github.com>
2023-10-24 17:56:41 +0530

    Time: 15 ms (63.16%), Space: 9.3 MB (9.94%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> m;
+        for(int i=0;i<magazine.length();i++){
+            m[magazine[i]]++;
+        }
+        
+        
+        for(auto e: m){
+            cout<<e.first<<" "<<e.second<<endl;
+        }
+        int i=0;
+        while(i<ransomNote.length()){
+            if(m.find(ransomNote[i]) == m.end()) return false;
+            
+            else{
+                 m[ransomNote[i]]--;
+                if(m[ransomNote[i]]==0) m.erase(ransomNote[i]);
+        
+            }
+            i++;
+        }
+        return true;
+        
+        
+    }
+};
