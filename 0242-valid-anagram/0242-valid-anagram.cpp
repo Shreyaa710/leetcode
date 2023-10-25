e7e253406396fb2b66ed75f1576530a864df5105
Shreya Sharma <110884745+Shreyaa710@users.noreply.github.com>
2023-10-25 22:57:02 +0530

    Time: 11 ms (46.42%), Space: 7.6 MB (50.12%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>m;
+        
+        if(s.length() != t.length()) return false;
+        for(int i=0;i<s.length();i++){
+            m[s[i]]++;
+        }
+        
+         for(int i=0;i<t.length();i++){
+            m[t[i]]--;
+             if(m[t[i]]==0) m.erase(t[i]);
+        }
+        
+        if(m.size()==0) return true;
+        
+        return false;
+    }
+};
