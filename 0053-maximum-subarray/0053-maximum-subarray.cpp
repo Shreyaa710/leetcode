9e0499f9e5e3acddbd1255ba45e086d9420e7d40
Shreya Sharma <110884745+Shreyaa710@users.noreply.github.com>
2023-12-22 10:33:01 +0530

    Time: 87 ms (44.45%), Space: 68.2 MB (27.64%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
     
-    int maxfind(int a,int b)
-    {
-        if(a>b)
-            return a;
+//     int maxfind(int a,int b)
+//     {
+//         if(a>b)
+//             return a;
         
-        else
-            return b;
-    }
+//         else
+//             return b;
+//     }
     
     int maxSubArray(vector<int>& nums) {
-       int max= nums[0];
-        int sum=nums[0];
-        for(int i=1;i<nums.size();i++)
-        {
-            if(nums[i]>nums[i]+sum){
-                sum= nums[i];
-            }
+//        int max= nums[0];
+//         int sum=nums[0];
+//         for(int i=1;i<nums.size();i++)
+//         {
+//             if(nums[i]>nums[i]+sum){
+//                 sum= nums[i];
+//             }
             
-            else
-                sum+=nums[i];
+//             else
+//                 sum+=nums[i];
             
-            max= maxfind(max,sum);
-        }
+//             max= maxfind(max,sum);
+//         }
         
-        return max;
+//         return max;
+        
+        int sum=nums[0];
+        int res=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(sum+nums[i]>nums[i]){
+                sum = sum+nums[i];
+                res=max(res,sum);
+            }
+            else{
+                sum=nums[i];
+                res=max(res,sum);
+            }
+        }
+        return res;
+        
+        
+        
     }
 };
