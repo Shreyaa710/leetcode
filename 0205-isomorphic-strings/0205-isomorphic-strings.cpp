44c39fa45f94a20ddefafd7f8aca0d3ec03e95c9
Shreya Sharma <110884745+Shreyaa710@users.noreply.github.com>
2023-12-30 11:55:35 +0530

    Time: 3 ms (91.65%), Space: 7.2 MB (98.90%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,28 +1,50 @@
-class Solution {
+// class Solution {
+// public:
+//     bool isIsomorphic(string s, string t) {
+//        map<char,int> s1;
+//        map<char,int> t1;
+        
+//         if(s.length() != t.length()) return false;
+        
+//         for(int i=0;i<s.length();i++){
+//             s1[s[i]]++;
+//         }
+        
+//          for(int i=0;i<t.length();i++){
+//             t1[t[i]]++;
+//         }
+        
+//         string res1,res2;
+        
+//         for(auto e:s1){
+//             res1+=to_string(e.second);
+//         }
+//         cout<<res1<<endl;
+        
+//         for(auto e:t1){
+//             res2+=to_string(e.second);
+//         }
+//         cout<<res2;
+        
+//         if(res1.compare(res2)==0) return true;
+   
+        
+//         return false;
+   
+//     }
+        
+        class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.length()==t.length())
+        int map_s[256] = { 0 };
+        int map_t[256] = { 0 };
+        int len = s.size();
+        for (int i = 0; i < len; ++i)
         {
-            int n=s.length();
-            unordered_map<char, char> s_to_t; 
-            unordered_map<char, char> t_to_s;
-
-        for (int i = 0; i < n; i++) 
-        {
-            if (s_to_t.find(s[i]) == s_to_t.end() && t_to_s.find(t[i]) == t_to_s.end()) 
-            {
-                s_to_t[s[i]] = t[i];
-                t_to_s[t[i]] = s[i];
-            } 
-            else {
-    
-                if (s_to_t[s[i]] != t[i] || t_to_s[t[i]] != s[i])           {
-                    return false;
-                }
-            }
+            if (map_s[s[i]]!=map_t[t[i]]) return false;
+            map_s[s[i]] = i+1;
+            map_t[t[i]] = i+1;
         }
-        return true;
-        }
-         return false;
+        return true; 
     }
 };
