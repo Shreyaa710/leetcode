e609cac7a2163e532803652aff9bef475c5120e1
Shreya Sharma <110884745+Shreyaa710@users.noreply.github.com>
2024-02-12 16:01:23 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (41.54%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,26 +1,25 @@
+
 class Solution {
 public:
-    
-    void help(int i,int target,int k,vector<int>&arr,vector<vector<int>>&ans,vector<int>&ds){
-        if(k==ds.size()){
-            if(target==0) ans.push_back(ds);
-            
+    void construct(int index, int k, int n, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds){
+        if(k==0) {
+            if(n==0) ans.push_back(ds);
             return;
         }
-        
-        for(int j=i;j<arr.size();j++){
-            if(arr[j]>target) break;
-            ds.push_back(arr[j]);
-            help(j+1,target-arr[j],k,arr,ans,ds);
+
+        for(int i=index;i<arr.size();i++)
+        {
+            if(arr[i]>n) break;
+            ds.push_back(arr[i]);
+            construct(i+1, k-1, n-arr[i], arr, ans, ds);
             ds.pop_back();
         }
-}
-    
-    vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> arr={1,2,3,4,5,6,7,8,9};
-        vector<vector<int>> ans;
-        vector<int>ds;
-        help(0,n,k,arr,ans,ds);
-        return ans;
     }
+    vector<vector<int>> combinationSum3(int k, int n) {
+       vector<int> arr = {1,2,3,4,5,6,7,8,9};
+       vector<vector<int>> ans;
+       vector<int> ds;
+       construct (0, k, n, arr, ans, ds) ;
+       return ans;
+    }
 };
