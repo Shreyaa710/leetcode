98f1f164bceb43379a1cf0496e9b9acf44847795
Shreya Sharma <110884745+Shreyaa710@users.noreply.github.com>
2024-02-16 15:48:31 +0530

    Time: 29 ms (21.95%), Space: 13.9 MB (17.44%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,47 +1,47 @@
-// class Solution {
-// public:
-    
-//     int max(int a,int b)
-//     {
-//         if(a>b) return a;
-        
-//         else return b;
-//     }
-    
-//     int lengthOfLongestSubstring(string s) {
-//         unordered_set<char>se;
-//         int i=0,j=0;
-//         int maxi=0;
-//         while(j<s.length())
-//         {
-//             if(se.find(s[j]) == se.end()){
-//                 se.insert(s[j]);
-//                 maxi=max(maxi,se.size());
-//                 j++;
-//             }
-//             else{
-//                 se.erase(s[i]);
-//                 i++;
-//             }
-//         }
-//         return maxi;
-//     }
-// };
-
 class Solution {
 public:
+    
+    int max(int a,int b)
+    {
+        if(a>b) return a;
+        
+        else return b;
+    }
+    
     int lengthOfLongestSubstring(string s) {
-        vector <int> count(128);
-        int ans=0;
-        int i=0;
-        for(int j=0; j<s.size();j++){
-            count[s[j]]++;
-
-            while(count[s[j]] > 1)
-            count[s[i++]]--;
-            
-            ans = max(ans,j-i+1);
+        unordered_set<char>se;
+        int i=0,j=0;
+        int maxi=0;
+        while(j<s.length())
+        {
+            if(se.find(s[j]) == se.end()){
+                se.insert(s[j]);
+                maxi=max(maxi,se.size());
+                j++;
+            }
+            else{
+                se.erase(s[i]);
+                i++;
+            }
         }
-        return ans;
+        return maxi;
     }
 };
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         vector <int> count(128);
+//         int ans=0;
+//         int i=0;
+//         for(int j=0; j<s.size();j++){
+//             count[s[j]]++;
+
+//             while(count[s[j]] > 1)
+//             count[s[i++]]--;
+            
+//             ans = max(ans,j-i+1);
+//         }
+//         return ans;
+//     }
+// };
