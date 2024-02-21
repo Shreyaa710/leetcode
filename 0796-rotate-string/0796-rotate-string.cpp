38e79e7d6b47930b2619177e9919641470335ff1
Shreya Sharma <110884745+Shreyaa710@users.noreply.github.com>
2024-02-21 09:52:43 +0530

    Time: 3 ms (41.45%), Space: 7.2 MB (59.03%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -3,7 +3,7 @@ public:
     bool rotateString(string s, string goal) {
         if(s.length() != goal.length()) return false;
         
-        if((s+s).find(goal) != string::npos) return true;
+        if((s+s).find(goal) != -1) return true;
         
         return false;
     }
