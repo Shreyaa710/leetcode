fda3ef72573b8fec10c2a5b2c6fefc1d8dc78fd9
Shreya Sharma <110884745+Shreyaa710@users.noreply.github.com>
2024-03-07 16:26:57 +0530

    Time: 9 ms (43.97%), Space: 21.1 MB (50.72%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,9 +4,7 @@ public:
         int i=0,temp;
         for(int j=1;j<nums.size();j++){
             if(nums[i]!=nums[j]){
-                temp=nums[i+1];
                 nums[i+1]=nums[j];
-                nums[j]=temp;
                 i++;
         }
         
