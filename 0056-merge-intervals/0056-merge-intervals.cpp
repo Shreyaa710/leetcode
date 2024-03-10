797a4dcdc35203aa74f7843d15b020a9b2bd9c90
Shreya Sharma <110884745+Shreyaa710@users.noreply.github.com>
2024-03-10 19:23:40 +0530

    Time: 20 ms (81.35%), Space: 23.2 MB (23.41%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,19 +4,17 @@ public:
         sort(intervals.begin(),intervals.end());
         
         vector<vector<int>> ans;
-        bool flag=false;
+
         
         for(int i=1;i<intervals.size();i++){
             
             if(intervals[i-1][1] >= intervals[i][0]){
                 intervals[i][0]=intervals[i-1][0];
                 intervals[i][1]=intervals[i][1]>intervals[i-1][1] ? intervals[i][1] : intervals[i-1][1];
-                    
-                flag= true;
+             
             }
             
             else{
-                flag=false;
                 vector<int> temp={intervals[i-1][0], intervals[i-1][1]};
                 ans.push_back(temp);
             } 
