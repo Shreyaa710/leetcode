00a18e8040cf26dfa97d0d8ab1f8fc4c1c19c84d
Shreya Sharma <110884745+Shreyaa710@users.noreply.github.com>
2024-03-10 10:42:13 +0530

    Time: 26 ms (49.01%), Space: 22.7 MB (36.00%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,35 +1,55 @@
+// class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         sort(intervals.begin(),intervals.end());
+        
+//         vector<vector<int>> ans;
+//         bool flag=false;
+        
+//         for(int i=1;i<intervals.size();i++){
+            
+//             if(intervals[i-1][1] >= intervals[i][0]){
+//                 intervals[i][0]=intervals[i-1][0];
+//                 intervals[i][1]=intervals[i][1]>intervals[i-1][1] ? intervals[i][1] : intervals[i-1][1];
+                    
+//                 flag= true;
+//             }
+            
+//             else{
+//                 flag=false;
+//                 vector<int> temp;
+//                 temp.push_back(intervals[i-1][0]);
+//                 temp.push_back(intervals[i-1][1]);
+                
+//                 ans.push_back(temp);
+//             } 
+//         }
+        
+//         vector<int> temp;
+//         temp.push_back(intervals[intervals.size()-1][0]);
+//         temp.push_back(intervals[intervals.size()-1][1]);
+        
+//         ans.push_back(temp);
+        
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
+        if (intervals.empty()) return {};
         
+        sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
-        bool flag=false;
         
-        for(int i=1;i<intervals.size();i++){
-            
-            if(intervals[i-1][1] >= intervals[i][0]){
-                intervals[i][0]=intervals[i-1][0];
-                intervals[i][1]=intervals[i][1]>intervals[i-1][1] ? intervals[i][1] : intervals[i-1][1];
-                    
-                flag= true;
+        for (const auto& interval : intervals) {
+            if (ans.empty() || ans.back()[1] < interval[0]) {
+                ans.push_back(interval);
+            } else {
+                ans.back()[1] = max(ans.back()[1], interval[1]);
             }
-            
-            else{
-                flag=false;
-                vector<int> temp;
-                temp.push_back(intervals[i-1][0]);
-                temp.push_back(intervals[i-1][1]);
-                
-                ans.push_back(temp);
-            } 
         }
-        
-        vector<int> temp;
-        temp.push_back(intervals[intervals.size()-1][0]);
-        temp.push_back(intervals[intervals.size()-1][1]);
-        
-        ans.push_back(temp);
         
         return ans;
     }
